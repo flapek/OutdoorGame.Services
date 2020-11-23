826146d030170b926ca71a545264efe0e882b892
flapek <filap4@gmail.com>
2020-11-23 10:55:07 +0100

    add cors policy

--- OutdoorGame.Services/Startup.cs
+++ OutdoorGame.Services/Startup.cs
@@ -27,6 +27,8 @@ namespace OutdoorGame.Services
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddCors();
+
             services.AddTransient<IQuestionRepository, QuestionRepository>();
             services.AddTransient<IGameRepository, GameRepository>();
 
@@ -49,9 +51,14 @@ namespace OutdoorGame.Services
 
             app.UseSwaggerDocs()
                 .UseErrorHandler();
-                //.UseMongo();
+            //.UseMongo();
 
             app.UseRouting();
+
+            app.UseCors(x => x
+                .AllowAnyOrigin()
+                .AllowAnyMethod()
+                .AllowAnyHeader());
 
             app.UseAuthorization();
 
